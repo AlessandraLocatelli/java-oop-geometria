af51f2e67e07acdad28ea0b5ce378c7ffc908538
Ales <alocatelli798@gmail.com>
2023-07-18 14:44:10 +0200

    done!

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,39 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+import org.w3c.dom.css.Rect;
+
+import java.util.Scanner;
+
+
+
+
+//chiedete all’utente di inserire, da console, i valori di base e di altezza
+// con cui istanziare un nuovo Rettangolo.
+// Dopo averlo istanziato, stampate a video il perimetro e l’area.
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Insert base: ");
+    int baseInput = Integer.parseInt(sc.nextLine());
+    System.out.println("Insert height: ");
+    int heightInput = Integer.parseInt(sc.nextLine());
+
+    Rectangle rectangle = new Rectangle(baseInput,heightInput);
+    System.out.println("Data: "+rectangle);
+    System.out.println("Area: "+rectangle.getArea());
+    System.out.println("Perimeter: "+rectangle.getPerimeter());
+
+
+
+
+
+
+
+    sc.close();
+    }
+
+
+
+
 }
--- src/org/lessons/java/geometria/Rectangle.java
+++ src/org/lessons/java/geometria/Rectangle.java
@@ -1,2 +1,60 @@
-package org.lessons.java.geometria;public class Rectangle {
+package org.lessons.java.geometria;
+
+// Classe con due attributi interi: base e altezza.
+// Aggiungere un opportuno costruttore con parametri.
+// Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano
+// e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+//BONUS Aggiungere alla classe Rettangolo un metodo disegna()
+// che stampa in console il rettangolo con le sue dimensioni.
+
+
+public class Rectangle {
+
+    //CAMPI
+    int base;
+    int height;
+
+
+    //COSTRUTTORI
+    Rectangle(int base, int height)
+    {
+        this.base = base;
+        this.height = height;
+
+    }
+
+
+    //METODI
+    int getArea()
+    {
+      int area = base * height;
+
+      return area;
+
+    }
+
+
+    int getPerimeter()
+    {
+
+        int perimeter = (base*2) + (height*2);
+
+        return perimeter;
+
+    }
+
+    //BONUS
+    void Draw()
+    {
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "base=" + base +
+                ", height=" + height +
+                '}';
+    }
 }
